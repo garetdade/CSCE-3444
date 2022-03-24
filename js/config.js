99d8947bfba79a7c0b5ea5869b4c41ccb8d29671
Garet Dade <61402950+garetdade@users.noreply.github.com>
2022-03-24 12:40:54 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,10 @@
-// info
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_mNwa4M2UT', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1tsoheu47n92p6eehbdvlqv6cb', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
