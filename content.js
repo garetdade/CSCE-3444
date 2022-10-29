ad2ea78755d03245a7b605f1c5a1ed94288e2f2b
atayyari <39579034+atayyari@users.noreply.github.com>
2022-10-29 02:13:26 -0500

    Update content.js

--- content.js
+++ content.js
@@ -1,31 +1,49 @@
 chrome.runtime.onMessage.addListener(gotMessage);
-function gotMessage(message,sender,sendresponse)
+
+function paragraphHighlighting()
 {
-	console.log(message.txt);
+	//Log for debugging only
+	console.log("Paragraph Highlighting Initiated...")
+
+	//Takes all "p" HTML elements and stores them in var paragraphs
 	let paragraphs = document.getElementsByTagName("p");
 	let count = 0;
+
+	//Loops through all p elements
 	for(par of paragraphs)
 	{
+		//Log for debugging only
 		console.log(count);
+
+		//Odd-even check to alternate paragraph shading
 		if (count % 2 == 0)
 		{
 			par.style['background-color'] = 'AntiqueWhite';
 		}
+
 		else 
 		{
 			par.style['background-color'] = 'Beige';
 		}
-		
+
 		par.style['color'] = 'black';
 		par.style['font-variant-ligatures'] = 'none';
 		count = count + 1;
 	}
+}
 
-	let headers3 = document.getElementsByTagName("h3");
-	for(hea of headers3)
-	{
-		hea.style['background-color'] = 'AntiqueWhite';
-		hea.style['color'] = 'black';
-		hea.style['font-variant-ligatures'] = 'none';
-	}
+function gotMessage(message,sender,sendresponse)
+{
+	//Relays the message that the listener picked up to the log
+	console.log(message.txt);
+
+	//Setting up toggleables
+	chrome.storage.local.get([
+		"highlighting"
+	], function(items) {
+		var highlighting = items.highlighting;
+		if (highlighting === true) {
+			paragraphHighlighting();
+		}
+	});
 }
