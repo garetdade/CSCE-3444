a284b9f175a51141a90a7f8a97489f5291a10248
atayyari <39579034+atayyari@users.noreply.github.com>
2022-10-29 16:31:52 -0500

    Added brightness contextMenus
    Currently nonfunctional

--- backgroundContext.js
+++ backgroundContext.js
@@ -5,6 +5,8 @@ chrome.contextMenus.create({
     "id": "speak"
 })
 
+//-----------------------------------
+
 chrome.contextMenus.create({
 	"title": "Image settings",
 	"contexts": ["image"],
@@ -37,4 +39,34 @@ chrome.contextMenus.create({
 	"contexts": ["image"],
 	"id": "50contrast",
 	"parentId": "imagecont"
+})
+
+// -----------------------------------
+
+chrome.contextMenus.create({
+	"title": "Adjust brightness",
+	"contexts": ["image"],
+	"id": "imagebright",
+	"parentId": "imagesettings"
+})
+
+chrome.contextMenus.create({
+	"title": "150%",
+	"contexts": ["image"],
+	"id": "150brightness",
+	"parentId": "imagebright"
+})
+
+chrome.contextMenus.create({
+	"title": "100%",
+	"contexts": ["image"],
+	"id": "100brightness",
+	"parentId": "imagebright"
+})
+
+chrome.contextMenus.create({
+	"title": "50%",
+	"contexts": ["image"],
+	"id": "50brightness",
+	"parentId": "imagebright"
 })
